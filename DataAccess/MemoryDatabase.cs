fc81c28b4098cf75de84a40f5de293f3ce23658c
wwolf <w.wolf7292@gmail.com>
2024-03-18 18:50:54 +0100

    Fist Commit

---
+++ DataAccess/MemoryDatabase.cs
@@ -0,0 +1,72 @@
+﻿using MemoryBusiness;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace MemoryDatabase
+{
+    public class DataBaseManager
+    {
+        private static string connectionString = "Server=localhost;Database=highscores;User ID=root;";
+
+        public void InsertResult(Player player)
+        {
+            using (MySqlConnection cnn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    cnn.Open();
+
+                    using (MySqlCommand command = new MySqlCommand("INSERT INTO highscores (Playername, Score, Moves) VALUES (@PlayerName, @Score, @moves)", cnn))
+                    {
+                        command.Parameters.AddWithValue("@PlayerName", player.PlayerName);
+                        command.Parameters.AddWithValue("@Score", player.Playerscore);
+                        command.Parameters.AddWithValue("@moves", player.Moves);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error inserting result: {e.Message}");
+                    // You may handle or log the exception here if required.
+                    throw; // Rethrowing the exception
+                }
+            }
+        }
+
+        public List<Player> Results()
+        {
+            List<Player> resultList = new List<Player>();
+
+            using (MySqlConnection cnn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    cnn.Open();
+
+                    using (MySqlCommand command = new MySqlCommand("SELECT * FROM highscores ORDER BY Score DESC LIMIT 10", cnn))
+                    {
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Player player = new Player(reader.GetString("Playername"), reader.GetInt32("Score"), reader.GetInt32("Moves"));
+                                resultList.Add(player);
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error retrieving results: {e.Message}");
+                    // You may handle or log the exception here if required.
+                    throw; // Rethrowing the exception
+                }
+            }
+
+            return resultList;
+        }
+    }
+}
---
+++ DataAccess/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MemoryBusiness;
+using MemoryDatabase;
+
+DataBaseManager a = new DataBaseManager();
+
+
+Player player = new Player("Wouter");
+
+player.Playerscore = 100;
+player.Moves = 10;
+a.InsertResult(player);
+
+
+foreach (var VARIABLE in a.Results())
+{
+    Console.WriteLine($"{VARIABLE.PlayerName} heeft een score behaald van {VARIABLE.Playerscore} met {VARIABLE.Moves} beurten");
+}
